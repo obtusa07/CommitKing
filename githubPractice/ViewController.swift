0722c402cca6337f9f2799022a138f5f6d949225
Taehwan Kim <obtusa07@gmail.com>
2023-02-04 22:20:17 +0900

    button created

--- githubPractice/ViewController.swift
+++ githubPractice/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.setTitle("github 로그인", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 30),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            loginButton.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
 
 }
 
