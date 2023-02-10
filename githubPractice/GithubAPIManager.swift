933b85778e568c56f63eaa3a75e1dbe15dd2492e
Taehwan Kim <obtusa07@gmail.com>
2023-02-10 14:24:37 +0900

    :sparkles: complete generate PAT

--- githubPractice/GithubAPIManager.swift
+++ githubPractice/GithubAPIManager.swift
@@ -15,11 +15,11 @@ class GithubAPIManager {
     // MARK: URL을 Scene에서 받아서 POST로 보내고 Token 받아오기
     // Json으로 보내면 아래와 같은 놈이 나올거
     // Accept: application/json
-//    {
-//      "access_token":"tesaknlsdf;osdjfobds;fbsnd;bsdfiansasdfasdfasdfsabdf",
-//      "scope":"repo,gist",
-//      "token_type":"bearer"
-//    }
+    //    {
+    //      "access_token":"tesaknlsdf;osdjfobds;fbsnd;bsdfiansasdfasdfasdfsabdf",
+    //      "scope":"repo,gist",
+    //      "token_type":"bearer"
+    //    }
     func tokenGenerate(url: URL) {
         guard let codeAndState = url.absoluteString.components(separatedBy: "code=").last else {
             preconditionFailure("Fail to find code in redirected url ")
@@ -29,55 +29,61 @@ class GithubAPIManager {
         guard let code = codeAndState.components(separatedBy: "&state=").first else {
             preconditionFailure("Cant't separeated code")
         }
-        
+        print(code)
         guard let state = codeAndState.components(separatedBy: "&state=").last else {
             preconditionFailure("Cant't separeated state")
         }
         // TODO: CSRF 공격을 방어하기 위하여 1차 통신 때 uuid와 돌아온 state가 같지 않으면 과정을 중단할 것. 순위가 낮다
-//        if state == ViewController.uuid {
-//            print("something is good")
-//        }
+        //        if state == ViewController.uuid {
+        //            print("something is good")
+        //        }
         
-        guard var components = URLComponents(string: GithubConfig.TOKENURL) else {
+        //        guard var components = URLComponents(string: GithubConfig.TOKENURL) else {
+        //            preconditionFailure("GithubConfig is broken. Fail to load TOKENURL")
+        //        }
+        
+        //        components.queryItems = [
+        //            URLQueryItem(name: "client_id", value: GithubConfig.CLIENT_ID),
+        //            URLQueryItem(name: "client_secret", value: GithubConfig.CLIENT_SECRET),
+        //            URLQueryItem(name: "code", value: code),
+        //            URLQueryItem(name: "redirect_uri", value: GithubConfig.REDIRECT_URI_LOGIN),
+        //            URLQueryItem(name: "state", value: state)
+        //        ]
+        
+        let param = ["client_id": GithubConfig.CLIENT_ID, "client_secret": GithubConfig.CLIENT_SECRET,
+                     "code": code, "redirect_uri":GithubConfig.REDIRECT_URI_LOGIN]
+        let paramData = try! JSONSerialization.data(withJSONObject: param, options: [])
+
+        guard let url = URL(string: GithubConfig.TOKENURL) else {
             preconditionFailure("GithubConfig is broken. Fail to load TOKENURL")
-        }
-        
-        components.queryItems = [
-            URLQueryItem(name: "client_id", value: GithubConfig.CLIENT_ID),
-            URLQueryItem(name: "client_secret", value: GithubConfig.CLIENT_SECRET),
-            URLQueryItem(name: "code", value: code),
-            URLQueryItem(name: "redirect_uri", value: GithubConfig.REDIRECT_URI_LOGIN),
-            URLQueryItem(name: "state", value: state)
-        ]
-        
-        guard let url = components.url else {
-            preconditionFailure("Code for token url Error")
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        request.httpBody = paramData
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(String(paramData.count), forHTTPHeaderField: "Content-Length")
+        let configuration = URLSessionConfiguration.ephemeral // Code는 Credential이라 판단
+
+        URLSession(configuration: configuration).dataTask(with: request) { data, response, error in
             guard let response = response as? HTTPURLResponse, (200..<300).contains(response.statusCode) else {
-                print("response: \(String(describing: response))")
-                return
+                preconditionFailure("Failed to receive Token response \(String(describing: response))")
             }
-            
             guard let data = data else {
-                return
+                preconditionFailure("Failed to receive Token Data")
             }
-            guard let result = String(data: data, encoding: .utf8) else {
-                preconditionFailure("Fail to generate Token result")
+            do {
+                let decoder = JSONDecoder()
+                let token = try decoder.decode(GithubTokenParser.self, from: data)
+                // MARK: KeyChain에 토큰 저장. encrytion 불가 문제로 UserDefaults 대신 채택
+                
+            } catch {
+                preconditionFailure("Can't decode Token json Data")
             }
-            print(result) //
-            
-            // MARK: UserDefaults에 토큰 저장
-            UserDefaults.standard.object(forKey: <#T##String#>)
-        }
-        task.resume()
-    
-    
+        }.resume()
+        
+        
     }
     
     func logout() {
