eb270ca4bb978e3ca6901b9b66dcef8d0fee31d8
Taehwan Kim <obtusa07@gmail.com>
2023-02-18 19:24:30 +0900

    :sparkles:  add Lottie at LoginView and downSampling extension

---
+++ CommitKing/Global/Extension/UIImage+extension.swift
@@ -0,0 +1,41 @@
+//
+//  UIImage+extension.swift
+//  CommitKing
+//
+//  Created by Taehwan Kim on 2023/02/18.
+//
+
+import UIKit
+
+extension UIImage {
+    // https://nsios.tistory.com/154
+    // 도입 보류. 메모리 용량과 관련해서 기술적 해결을 한 경험을로 넣어야 함
+    func resize(newWidth: CGFloat) -> UIImage {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { context in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        print("화면 배율: \(UIScreen.main.scale)")// 배수
+        print("origin: \(self), resize: \(renderImage)")
+        return renderImage
+    }
+    func downSampling(imageAt imageURL: URL, to pointSize: CGSize, scale: CGFloat) -> UIImage {
+      let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+      let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
+
+      let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+      let downSamplingOptions = [
+        kCGImageSourceCreateThumbnailFromImageAlways: true,
+        kCGImageSourceShouldCacheImmediately: true,
+        kCGImageSourceCreateThumbnailWithTransform: true,
+        kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+      ] as CFDictionary
+
+      let downSampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downSamplingOptions)!
+
+      return UIImage(cgImage: downSampledImage)
+    }
+}
--- CommitKing/Views/LoginViewController.swift
+++ CommitKing/Views/LoginViewController.swift
@@ -6,15 +6,31 @@
 //
 
 import UIKit
-import WebKit
+import Lottie
 
 class LoginViewController: UIViewController {
     // TODO: 여기서 메인 로티 이미지 삽입할 예정, 로티 작업 거치기 전에 UI 작업으로 비어 있는 이미지 넣기
+    private let loginCodingImageView: LottieAnimationView = {
+        let imageView = LottieAnimationView(name: "LoginCodingImage")
+        imageView.loopMode = .loop
+        imageView.contentMode = .scaleAspectFit
+        imageView.play()
+        return imageView
+    }()
     private let loginButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .black
-        button.tintColor = .white
-        button.setTitle("github 로그인", for: .normal)
+        // MARK: - UIButton configuration 방식은 iOS 15+
+        // configuration의 치명적인 단점은 버튼에 적합한 사이즈의 이미지를 리사이징 하는 기능이 없다는 것이다.
+        // WWDC 2018에서 추천한 UIGraphicsImageRenderer을 이용하여 문제 해결 시도하겠음
+        var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(named: "github-mark")
+        configuration.imagePlacement = .leading
+        configuration.imagePadding = 5
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        configuration.title = "github 로그인"
+        configuration.baseBackgroundColor = .black
+        configuration.baseForegroundColor = .white
+        configuration.cornerStyle = .dynamic
+        let button = UIButton(configuration: configuration)
         return button
     }()
     override func viewDidLoad() {
@@ -27,9 +43,17 @@ class LoginViewController: UIViewController {
         view.backgroundColor = .white
         // MARK: - LoginButton 세팅
         view.addSubview(loginButton)
+        view.addSubview(loginCodingImageView)
+        loginCodingImageView.translatesAutoresizingMaskIntoConstraints = false
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            loginButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 30),
+            loginCodingImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            loginCodingImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            loginCodingImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            loginCodingImageView.heightAnchor.constraint(equalToConstant: 500)
+        ])
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: loginCodingImageView.bottomAnchor, constant: 30),
             loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             loginButton.heightAnchor.constraint(equalToConstant: 50)
@@ -40,6 +64,7 @@ class LoginViewController: UIViewController {
         loginButton.addTarget(self, action: #selector(goHome), for: .touchUpInside)
     }
     @objc private func goHome() {
+    #warning(": [BUG] 로그인이 실제로 안 되도 자동으로 넘어가는 문제가 있음 따로 행동을 정의하기 보다 GithubAPIManager에서 처리하는게 합리적일듯")
         GithubAPIManager.loginButtonClicked()
         let viewController = MainViewController()
         viewController.modalPresentationStyle = .fullScreen
