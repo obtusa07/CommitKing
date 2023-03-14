7e34eab160f64997ec7a46323dabf24699bf9de3
Taehwan Kim <obtusa07@gmail.com>
2023-03-14 15:42:49 +0900

    :art: relocate code files
    VM, V, VC를 추가하고 위치 조정

--- CommitKing/Global/Extension/UIView+extension.swift
+++ CommitKing/Global/Extension/UIView+extension.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIView {
     // MARK: Subviews를 한줄에서 선언 가능
-    func addSubviews(_ views: UIView...){
+    func addSubviews(_ views: UIView...) {
         views.forEach {
             addSubview($0)
         }
